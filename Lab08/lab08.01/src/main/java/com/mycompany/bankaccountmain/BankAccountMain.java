a75ca082165f673b08b10aa0cb92050b836b850d
umashiparindya <140793038+umashiparindya@users.noreply.github.com>
2023-07-30 11:54:20 +0530

    Add files via upload

---
+++ Lab08/lab08.01/src/main/java/com/mycompany/bankaccountmain/BankAccountMain.java
@@ -0,0 +1,8 @@
+package com.mycompany.bankaccountmain;
+public class BankAccountMain {
+
+    public static void main(String[] args)
+    {
+        
+    }
+}
---
+++ Lab08/lab08.02/src/main/java/com/mycompany/shapemain/ShapeMain.java
@@ -0,0 +1,8 @@
+package com.mycompany.shapemain;
+public abstract class ShapeMain {
+
+    public static void main(String[] args)
+    {
+        
+    }
+}
