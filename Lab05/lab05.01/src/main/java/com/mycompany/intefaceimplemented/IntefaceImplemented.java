c8f32e3169c55ac61c2e41e7f7a9e22c13231373
umashiparindya <140793038+umashiparindya@users.noreply.github.com>
2023-07-30 11:50:26 +0530

    Add files via upload

---
+++ Lab05/lab05.01/src/main/java/com/mycompany/intefaceimplemented/IntefaceImplemented.java
@@ -0,0 +1,8 @@
+package com.mycompany.intefaceimplemented;
+public class IntefaceImplemented implements MyFirstInterface 
+{
+    public void display() 
+    {
+        System.out.println("Value of x: " + x);
+    }
+}
---
+++ Lab05/lab05.01/src/main/java/com/mycompany/intefaceimplemented/MyFirstInterface.java
@@ -0,0 +1,6 @@
+package com.mycompany.intefaceimplemented;
+public interface MyFirstInterface 
+{
+    int x = 10;
+    void display();
+}
