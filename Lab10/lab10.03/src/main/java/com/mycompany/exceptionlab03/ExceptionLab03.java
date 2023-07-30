278d878cdf2775c48eabafece3a0daa633c9d0c3
umashiparindya <140793038+umashiparindya@users.noreply.github.com>
2023-07-30 11:55:47 +0530

    Add files via upload

---
+++ Lab10/lab10.03/src/main/java/com/mycompany/exceptionlab03/ExceptionLab03.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exceptionlab03;
+
+/**
+ *
+ * @author Intel
+ */
+public class ExceptionLab03 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
